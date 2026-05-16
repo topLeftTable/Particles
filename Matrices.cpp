e4b658bc20a4b26110697aa4f3f4c1013b2fe2da
LoganMD <logan@logandev.org>
2026-05-15 21:25:20 -0700

    delegated tasks

--- Matrices.cpp
+++ Matrices.cpp
@@ -3,46 +3,46 @@ using namespace Matrices;
 
 Matrix::Matrix(int _rows, int _cols)
 {
-	// YOUR CODE HERE !!!
+	// dd YOUR CODE HERE !!!
 }
 
 Matrix operator+(const Matrix &a, const Matrix &b)
 {
-	// YOUR CODE HERE !!!
+	// wk YOUR CODE HERE !!!
 }
 
 Matrix operator*(const Matrix &a, const Matrix &b)
 {
-	// YOUR CODE HERE !!!
+	// ld YOUR CODE HERE !!!
 }
 
 bool operator==(const Matrix &a, const Matrix &b)
 {
-	// YOUR CODE HERE !!!
+	// dd YOUR CODE HERE !!!
 }
 
 bool operator!=(const Matrix &a, const Matrix &b)
 {
-	// YOUR CODE HERE !!!
+	// wk YOUR CODE HERE !!!
 }
 
 ostream &operator<<(ostream &os, const Matrix &a)
 {
-	// YOUR CODE HERE !!!
+	// ld YOUR CODE HERE !!!
 }
 
 RotationMatrix::RotationMatrix(double theta) : Matrix(getRows(), getCols())
 {
-	// YOUR CODE HERE !!!
+	// dd YOUR CODE HERE !!!
 }
 
 ScalingMatrix::ScalingMatrix(double scale) : Matrix(getRows(), getCols())
 {
-	// YOUR CODE HERE !!!
+	// wk YOUR CODE HERE !!!
 }
 
 TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(getRows(), getCols())
 {
-	// YOUR CODE HERE !!!
+	// ld YOUR CODE HERE !!!
 }
 
