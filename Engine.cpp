9a173b4005fb07c464d6c9704c7b371dda288ba1
deathsdebtor <deathsdebtor@users.noreply.github.com>
2026-05-19 17:40:29 -0700

    Add Copyright attributions

--- Engine.cpp
+++ Engine.cpp
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2022-2025 Ryan Hermle <rhermle>
+ * Modified Works by LoganMD, WaitingKeptYouHuh, and deathsdebtor
+ * Copyright (C) 2026 topLeftTable
+
+Particles is free software: you can redistribute it and/or modify
+its terms under the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Particles is distributed in the hope that it will be useful,
+but WITHOUT WARRANTY. See <https://www.gnu.org/licenses/> for details.
+**/
+
 #include "Engine.h"
 #include <random>
 
